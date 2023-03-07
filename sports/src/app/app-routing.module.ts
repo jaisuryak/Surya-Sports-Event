043bef58a9b27b091cf5cb9f0e5727ea6fda4269
Sam M <samsolomonm@teezle.com>
2023-03-07 20:06:05 +0000

    inital commit Routing side

--- sports/src/app/app-routing.module.ts
+++ sports/src/app/app-routing.module.ts
@@ -1,7 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LoginComponent } from './authentication/login/login.component';
+import { RegistrationComponent } from './authentication/registration/registration.component';
+// import { TableComponent } from './layout/table/table.component';
+import { SidenavComponent } from './sidenav/sidenav.component';
+import { TableComponent } from './sports/table/table.component';
 
-const routes: Routes = [];
+export const routes: Routes = [
+  { path: 'login', component: LoginComponent },
+  { path: 'register', component: RegistrationComponent },
+  { path: 'sidenav', component: SidenavComponent },
+  { path: 'table', component:TableComponent }
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- sports/src/app/app.module.ts
+++ sports/src/app/app.module.ts
@@ -1,18 +1,33 @@
-import { NgModule } from '@angular/core';
+import { NgModule, isDevMode } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
-import { AppRoutingModule } from './app-routing.module';
+import { AuthenticationModule } from './authentication/authentication.module';
+import { AppRoutingModule, routes } from './app-routing.module';
 import { AppComponent } from './app.component';
-
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { RouterModule, Routes } from '@angular/router';
+import { SidenavComponent } from './sidenav/sidenav.component';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { ReactiveFormsModule } from '@angular/forms';
+import { MatListModule } from '@angular/material/list';
+import { MatIconModule } from '@angular/material/icon';
+import { MatMenuModule } from '@angular/material/menu';
+import { SportsModule } from './sports/sports.module';
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [AppComponent, SidenavComponent],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    MatMenuModule,
+    SportsModule,
+    MatListModule,
+    MatIconModule,
+    AppRoutingModule,
+    MatSidenavModule,
+    ReactiveFormsModule,
+    BrowserAnimationsModule,
+    AuthenticationModule,
+    RouterModule.forRoot(routes),
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
